17b23cfe53f8b459763b3bf5eafd3d2a65f4c67e
mlzboy <15268112200@163.com>
2023-05-01 10:26:06 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+def doSomething(text: str, bool2: bool, num: number, list2: List[any], sprite: game.LedSprite, image: Image):
+    return 0
+input.set_sound_threshold(SoundThreshold.LOUD, input.rotation(Rotation.PITCH))
+led.toggle(0, 0)
+radio.set_transmit_serial_number(False)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,9 @@
+function doSomething (text: string, bool: boolean, num: number, list: any[], sprite: game.LedSprite, image: Image) {
+    return 0
+}
+input.setSoundThreshold(SoundThreshold.Loud, input.rotation(Rotation.Pitch))
+led.toggle(0, 0)
+radio.setTransmitSerialNumber(false)
+basic.forever(function () {
+	
+})
